1a71f0558c00987a2f70db10208e3e4ad5a133be
Sinyeol An <soundlake@gmail.com>
2014-08-07 01:10:52 +0200

    solve p5

---
+++ problem005/sol.py
@@ -0,0 +1,34 @@
+primes = [2, 3, 5, 7, 11, 13, 17, 19]
+factors = {}
+for p in primes:
+    factors[str(p)] = 0
+
+
+
+def factorize(n):
+    d = {}
+    if n in primes:
+        d[str(n)] = 1
+    else:
+        for p in primes:
+            while n % p == 0:
+                if str(p) in d:
+                    d[str(p)] += 1
+                else:
+                    d[str(p)] = 1
+                n /= p
+    return d
+
+
+
+# main logic
+for i in range(2, 21):
+    factorized = factorize(i);
+    for k in factorized:
+        if (factors[k] < factorized[k]):
+            factors[k] = factorized[k]
+print factors
+answer = 1
+for k in factors:
+    answer *= int(k) ** factors[k]
+print answer
