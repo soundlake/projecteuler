237ca230f1178ef74a20ccfb19691cd0aa41db83
Sinyeol An <soundlake@gmail.com>
2024-11-20 15:07:45 +0100

    Add time measure to the solution

--- problem003/rust/src/main.rs
+++ problem003/rust/src/main.rs
@@ -1,4 +1,7 @@
-fn main() {
+// https://stackoverflow.com/a/44378174/4276533 for measuring time
+use std::time::{SystemTime, UNIX_EPOCH};
+
+fn solution() {
     let target = 600851475143;
     let max = (target as f64).sqrt().floor() as u64;
     let mut pool: Vec<u64> = (2..(max + 1)).collect();
@@ -13,4 +16,15 @@ fn main() {
     }
 
     println!("The answer is: {}", factors[factors.len() - 1]);
+}
+
+fn main() {
+    let start = SystemTime::now();
+
+    solution();
+
+    let since_the_epoch = start
+        .duration_since(UNIX_EPOCH)
+        .expect("Time went backwards");
+    println!("It took {:?}", since_the_epoch);
 }
