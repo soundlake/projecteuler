d05d01aa9bc460f02f594999b1b6353b265fac80
Sinyeol An <soundlake@gmail.com>
2024-11-28 00:47:27 +0100

    Refactor P001
    Make it testable.

--- problem001/rust/src/main.rs
+++ problem001/rust/src/main.rs
@@ -1,11 +1,25 @@
-fn main() {
+fn get_answer(threshold: u32) -> u32 {
     let mut sum:u32 = 0;
 
-    for number in 1..1000 {
+    for number in 1..threshold {
         if number % 3 == 0 || number % 5 == 0 {
             sum += number;
         }
     }
 
-    println!("The answer is: {}", sum);
+    sum
+}
+
+fn main() {
+    println!("The answer is: {}", get_answer(1000));
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_get_answer() {
+        assert_eq!(get_answer(10), 23);
+    }
 }
