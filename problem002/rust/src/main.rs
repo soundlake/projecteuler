dc5d3d6cfe32c34c8f3ff0f78bfcab04c836b955
Sinyeol An <soundlake@gmail.com>
2024-11-28 00:59:57 +0100

    Refactor P002
    Following rust idiom

--- problem002/rust/src/main.rs
+++ problem002/rust/src/main.rs
@@ -1,26 +1,20 @@
 fn fibs(max: u32) -> Vec<u32> {
     // init fib vector
-    let mut v: Vec<u32> = Vec::new();
-    v.push(1);
-    v.push(2);
+    let mut v: Vec<u32> = [1, 2].to_vec();
 
-    let mut last = v[v.len() - 1];
-    while last < max{
+    while v[v.len() - 1] < max {
         v.push(v[v.len() - 1] + v[v.len() - 2]);
-        last = v[v.len() - 1];
     }
 
     v
 }
 
 fn main() {
-    let mut answer = 0;
-
-    for fib in fibs(4_000_000) {
-        if fib % 2 == 0 {
-            answer += fib;
-        }
-    }
+    let answer = fibs(4_000_000)
+        .into_iter()
+        .filter(| f | f % 2 == 0)
+        .reduce(| a, b | a + b)
+        .unwrap();
 
     println!("The answer is: {}", answer);
 }
