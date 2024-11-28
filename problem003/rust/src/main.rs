f7d3cfb2fafc35bcc385a937071a3d7b0767d48e
Sinyeol An <soundlake@gmail.com>
2024-11-28 01:34:27 +0100

    Refactor P003
    - Make it testable
    - Make the time measure a macro

--- problem003/rust/src/main.rs
+++ problem003/rust/src/main.rs
@@ -1,30 +1,75 @@
 // https://stackoverflow.com/a/44378174/4276533 for measuring time
-use std::time::{SystemTime, UNIX_EPOCH};
+use std::time::SystemTime;
 
-fn solution() {
-    let target = 600851475143;
+macro_rules! measure_time {
+    ($f:ident($($arg:expr),*)) => {{
+        println!("Measuring the function: {}", stringify!($f));
+        let start = SystemTime::now();
+
+        let return_value = $f($($arg),*);
+
+        let time_elapsed = SystemTime::now()
+            .duration_since(start)
+            .expect("Time went backwards");
+        println!("It took {:?}", time_elapsed);
+
+        return_value
+    }};
+}
+
+fn find_prime_factors(target: u64) -> Vec<u64> {
     let max = (target as f64).sqrt().floor() as u64;
-    let mut pool: Vec<u64> = (2..(max + 1)).collect();
-    let mut factors: Vec<u64> = Vec::new();
 
-    while pool.len() > 0 {
-        let candidate = pool[0];
-        if target % candidate == 0 {
-            factors.push(candidate);
+    match target {
+        0 | 1=> [].to_vec(),
+        _ => {
+            let mut pool: Vec<u64> = (2..(max + 1)).collect();
+            let mut factors: Vec<u64> = Vec::new();
+
+            while pool.len() > 0 {
+                let candidate = pool[0];
+                if target % candidate == 0 {
+                    factors.push(candidate);
+                }
+                pool.retain(|i| i % candidate != 0);
+            }
+
+            // target is a prime number
+            if factors.len() == 0 {
+                return [target].to_vec();
+            }
+
+            factors
         }
-        pool.retain(|i| i % candidate != 0);
     }
+}
 
-    println!("The answer is: {}", factors[factors.len() - 1]);
+fn get_answer(target: u64) -> u64 {
+    let factors = find_prime_factors(target);
+
+    factors[factors.len() - 1]
 }
 
 fn main() {
-    let start = SystemTime::now();
+    let answer = measure_time!(get_answer(600851475143));
 
-    solution();
+    println!("The answer is: {}", answer);
+}
 
-    let since_the_epoch = start
-        .duration_since(UNIX_EPOCH)
-        .expect("Time went backwards");
-    println!("It took {:?}", since_the_epoch);
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_find_prime_factors() {
+        assert_eq!(find_prime_factors(2), [2].to_vec());
+        assert_eq!(find_prime_factors(3), [3].to_vec());
+        assert_eq!(find_prime_factors(4), [2].to_vec());
+        assert_eq!(find_prime_factors(13195), [5, 7, 13, 29].to_vec());
+    }
+
+    #[test]
+    fn test_get_answer() {
+        assert_eq!(get_answer(13195), 29);
+    }
 }
