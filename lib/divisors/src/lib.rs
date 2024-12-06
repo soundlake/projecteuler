3e62c64deba920430842b898d0c4612cf04c0c98
Sinyeol An <soundlake@gmail.com>
2024-12-06 01:54:46 +0100

    Refactor: Make divisors related funciton generic
    It was for `u16` and there was "attempt to add with overflow". So, it
    has to be changed to accept `u32` or bigger. But, it also keep the
    backward compatability. Making it generic was a good option although it
    is dirty and ugly.

--- lib/divisors/src/lib.rs
+++ lib/divisors/src/lib.rs
@@ -1,28 +1,34 @@
-pub fn find_proper_divisors(n: u16) -> Result<Vec<u16>, String> {
-    let mut divisors: Vec<u16> = [].to_vec();
+use num::{ FromPrimitive, ToPrimitive, Unsigned };
 
-    match n {
+const PERR: &str = "parse error";
+
+pub fn find_proper_divisors<T: Unsigned + FromPrimitive + ToPrimitive + std::cmp::Ord + Copy>(n: T) -> Result<Vec<T>, String> {
+    let mut divisors: Vec<T> = Vec::new();
+
+    match n.to_u32().expect(PERR) {
         0 => return Err("Any number can be divisor of zero.".to_string()),
         1 => return Ok(divisors),
-        _ => divisors.push(1),
+        _ => divisors.push(T::from_u32(1).expect(PERR)),
     }
 
-    for i in 2..=((n as f32).sqrt().floor() as u16) {
-        if n % i == 0 {
+    let mut i: T = T::from_u32(2).expect(PERR);
+    while i <= (T::from_f32(T::to_f32(&n).expect(PERR).sqrt().floor()).expect(PERR)) {
+        if n % i == T::from_u32(0).expect(PERR) {
             let d = n / i;
             divisors.push(i);
             if i != d {
                 divisors.push(d);
             }
         }
+        i = i + T::from_u32(1).expect(PERR);
     }
     divisors.sort();
     Ok(divisors)
 }
 
-pub fn sum_of_proper_divisors(n: u16) -> Result<u16, String> {
+pub fn sum_of_proper_divisors<T: Unsigned + FromPrimitive + ToPrimitive + std::cmp::Ord + Copy>(n: T) -> Result<T, String> {
     match find_proper_divisors(n)?.into_iter().reduce(|a, b| a + b) {
-        None => Ok(0),
+        None => Ok(T::from_u32(0).expect(PERR)),
         n => return Ok(n.unwrap()),
     }
 }
@@ -33,19 +39,19 @@ mod tests {
 
     #[test]
     fn test_find_proper_divisors() {
-        assert!(find_proper_divisors(0).is_err());
-        assert_eq!(find_proper_divisors(1).unwrap(), [].to_vec());
-        assert_eq!(find_proper_divisors(2).unwrap(), [1].to_vec());
-        assert_eq!(find_proper_divisors(10).unwrap(), [1, 2, 5].to_vec());
-        assert_eq!(find_proper_divisors(220).unwrap(), [1, 2, 4, 5, 10, 11, 20, 22, 44, 55, 110].to_vec());
+        assert!(find_proper_divisors(0u8).is_err());
+        assert_eq!(find_proper_divisors(1u8).unwrap(), [].to_vec());
+        assert_eq!(find_proper_divisors(2u16).unwrap(), [1].to_vec());
+        assert_eq!(find_proper_divisors(10u32).unwrap(), [1, 2, 5].to_vec());
+        assert_eq!(find_proper_divisors(220u64).unwrap(), [1, 2, 4, 5, 10, 11, 20, 22, 44, 55, 110].to_vec());
     }
 
     #[test]
     fn test_sum_of_proper_divisors() {
-        assert!(sum_of_proper_divisors(0).is_err());
-        assert_eq!(sum_of_proper_divisors(1).unwrap(), 0);
-        assert_eq!(sum_of_proper_divisors(2).unwrap(), 1);
-        assert_eq!(sum_of_proper_divisors(10).unwrap(), 8);
-        assert_eq!(sum_of_proper_divisors(220).unwrap(), 284);
+        assert!(sum_of_proper_divisors(0u8).is_err());
+        assert_eq!(sum_of_proper_divisors(1u8).unwrap(), 0);
+        assert_eq!(sum_of_proper_divisors(2u16).unwrap(), 1);
+        assert_eq!(sum_of_proper_divisors(10u32).unwrap(), 8);
+        assert_eq!(sum_of_proper_divisors(220u64).unwrap(), 284);
     }
 }
